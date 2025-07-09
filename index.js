ae737a06045756ec9be03c383690d33c35341b50
Vijay Singh <thakurvijayofficial@gmail.com>
2025-07-09 22:24:41 +0530

    Fixed CORS

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 const allowedOrigins = [
   "http://localhost",
   "https://theajthakur.vercel.app",
-  "https://treshop.vercel.app/",
+  "https://treshop.vercel.app",
 ];
 
 app.use(
